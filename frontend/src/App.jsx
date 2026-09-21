e2c9ff5a445a2a6d923439c1df53bc8294457a2f
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2026-09-21 07:17:44 -0300

    feat(frontend): initial setup (#11)
    - set up React with Vite, ESLint and Prettier; React Router in declarative mode; and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router';
+
+function App() {
+  return (
+    <>
+      <h1>Adestração de Animais</h1>
+      <Link to="/login">Login</Link>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router';
+
+function Login() {
+  return (
+    <>
+      <h1>Login</h1>
+      <Link to="/">Home</Link>
+    </>
+  );
+}
+
+export default Login;
